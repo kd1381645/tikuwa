ce31455dcd815afb5b9767fdc260d7c70bec4b82
kd1381645 <kd1381645@st.kobedenhi.ac.jp>
2026-07-23 15:08:29 +0900

    commit

--- JammuGame/Src/Application/Object/TitleObject/Switch/Switch.cpp
+++ JammuGame/Src/Application/Object/TitleObject/Switch/Switch.cpp
@@ -1,16 +1,16 @@
-﻿#include "Switch.h"
-
-void Switch::Init()
-{
-	m_tex = std::make_shared<KdTexture>();
-	m_tex->Load("Asset/Textures/Title/start.png");
-
-}
-
-
-
-void Switch::DrawSprite()
-{
-	KdShaderManager::Instance().m_spriteShader.DrawTex(m_tex, -220, -130, 500,250 );
-}
-
+﻿//#include "Switch.h"
+//
+//void Switch::Init()
+//{
+//	m_tex = std::make_shared<KdTexture>();
+//	m_tex->Load("Asset/Textures/Title/start.png");
+//
+//}
+//
+//
+//
+//void Switch::DrawSprite()
+//{
+//	KdShaderManager::Instance().m_spriteShader.DrawTex(m_tex, -220, -130, 500,250 );
+//}
+//
